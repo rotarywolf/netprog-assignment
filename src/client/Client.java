ddbc10a433a23b3bfc0d4b8a53d92a4a9e1b6f54
rotarywolf <speck@tfwno.gf>
2019-05-16 13:50:43 +1000

    basic bitch client

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,10 +1,18 @@
 package client;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException, IOException {
 		// TODO Auto-generated method stub
-
+		testMain();
+	}
+	
+	public static void testMain() throws UnknownHostException, IOException {
+		Socket socket = new Socket("localhost", 61802);
 	}
 
 }
